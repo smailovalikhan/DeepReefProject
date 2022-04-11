d43ba819eefbb5818f60aac263fb1b1c902af7cd
smailovalikhan <103212292+smailovalikhan@users.noreply.github.com>
2022-04-12 00:03:47 +0300

    Delete DeepReef-newDesign directory

--- DeepReef-newDesign/js/scrollIndicator.js
+++
@@ -1,6 +0,0 @@
-const scrollIndicator = document.querySelector('#scroll-indicator'),
-      k = (window.innerHeight - 120) / scrollMaxY
-
-document.addEventListener('scroll', () => {
-  scrollIndicator.style.top = scrollY * k + 'px'
-})
